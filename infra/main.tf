8126db4f2ba856303bebb0caa002c0ed1b71fe41
PurvaShinde21 <purvapshinde.21@gmail.com>
2025-11-06 14:37:31 +0000

    Initial Terraform setup with .gitignore

---
+++ infra/main.tf
@@ -0,0 +1,110 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# ------------------------------
+# Resource Group
+# ------------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-rg"
+  location = "centralindia"
+}
+
+# ------------------------------
+# Virtual Network
+# ------------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "devops-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# ------------------------------
+# Subnet (depends on VNet)
+# ------------------------------
+resource "azurerm_subnet" "subnet" {
+  name                 = "devops-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  depends_on = [azurerm_virtual_network.vnet]
+}
+
+# ------------------------------
+# Public IP (Standard SKU)
+# ------------------------------
+resource "azurerm_public_ip" "pubip" {
+  name                = "devops-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# ------------------------------
+# Network Interface
+# ------------------------------
+resource "azurerm_network_interface" "nic" {
+  name                = "devops-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pubip.id
+  }
+}
+
+# ------------------------------
+# Linux Virtual Machine
+# ------------------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "devops-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  depends_on = [azurerm_network_interface.nic]
+}
+
+# ------------------------------
+# Output the public IP
+# ------------------------------
+output "vm_public_ip" {
+  value = azurerm_public_ip.pubip.ip_address
+  description = "Public IP of the deployed VM"
+}
