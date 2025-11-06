3973427d9f2fbbf4e9d04c693e94935e9b20c716
PurvaShinde21 <purvapshinde.21@gmail.com>
2025-11-06 15:28:37 +0000

    Added style.css and script.js for frontend

---
+++ app/script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("clickMe");
+  button.addEventListener("click", () => {
+    alert("🚀 Deployed successfully via Azure DevOps CI/CD!");
+  });
+});
